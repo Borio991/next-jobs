9b35c4ba078b3e49787249e89646da48cde0bad1
eslam sami <eslamsami9911@gmail.com>
2023-09-29 01:38:07 +0300

    full url error

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,7 +9,7 @@ export default Admin;
 
 export async function getServerSideProps(context: any) {
   const session = await getServerSession(context.req, context.res, authOptions);
-  const res = await fetch("http://localhost:3000/api/hello", {
+  const res = await fetch("/api/hello", {
     method: "GET",
     headers: {
       authorization: `Bearer ${session?.user.accessToken}`,
